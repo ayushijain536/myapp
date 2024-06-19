4e70c5125ae47182db897bfaec139e5a019039d0
Ayushi Jain <jainsab3756@gmail.com>
2024-06-19 13:15:13 +0530

    update day2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,41 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a className="navbar-brand" href="/">Textutils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link" href="/" aria-current="page">Features</a>
         </li>
-       
-    
+        <li className="nav-item">
+          <a className="nav-link" href="/" aria-current="page">Profile</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="/" aria-current="page">Contact</a>
+        </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
+
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+
