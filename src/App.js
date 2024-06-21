97f1ae8c533e17013e613e7cf4229d2990a63947
Ayushi Jain <jainsab3756@gmail.com>
2024-06-21 21:42:46 +0530

    Add textarea

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
 function App() {
   return (
    <>
-   <Navbar/>
-   
+   <Navbar title='TextConverter'/>
+   <div className="container my-3">
+       <TextForm title='Convert Your Text'></TextForm>
+   </div>
    </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <>
        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Textutils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/" aria-current="page">Features</a>
-        </li>
-        <li className="nav-item">
           <a className="nav-link" href="/" aria-current="page">Profile</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="/" aria-current="page">Contact</a>
         </li>
       </ul>
     </div>
-  </div>
+   </div>
 </nav>
 
     </>
